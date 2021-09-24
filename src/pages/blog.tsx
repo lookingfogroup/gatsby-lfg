a3ac7d73389ed8ce9b2ac9d0bb50c33f69ce0975
Penn Porterfield <penn@wintreetz.com>
2021-09-24 15:00:50 -0400

    Added it and got it working on the new Gat site w/ AWS Amplify so amazing need to upgrade to 2021+

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import Unity, { UnityContext } from "react-unity-webgl";
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
 import Posts from 'components/Posts';
 
+
+const unityContext = new UnityContext({
+  loaderUrl: "https://dev.d2kb8d5702060z.amplifyapp.com/Build/pfdg.loader.js",
+  dataUrl: "https://dev.d2kb8d5702060z.amplifyapp.com/Build/pfdg.data",
+  frameworkUrl: "https://dev.d2kb8d5702060z.amplifyapp.com/Build/pfdg.framework.js",
+  codeUrl: "https://dev.d2kb8d5702060z.amplifyapp.com/Build/pfdg.wasm",
+});
+
 const BlogPage: React.FC = () => {
   return (
+    
     <Layout>
       <SEO title="Blog" />
-      <Posts />
+      <Unity
+        unityContext={unityContext}
+        matchWebGLToCanvasSize={false}
+        style={{ width: "500px", height: "500px" }}
+    />
     </Layout>
   );
 };
